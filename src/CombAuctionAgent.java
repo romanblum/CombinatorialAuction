bd258d4fb2be727db7b8280d2876aef3b1196908
snathan <snathan@cs.brown.edu>
2018-05-05 17:56:31 -0400

    sean

--- src/CombAuctionAgent.java
+++ src/CombAuctionAgent.java
@@ -46,8 +46,8 @@ public class CombAuctionAgent extends AbsCombinatorialProjectAgentV2 {
 		for (int i = 0; i < 14; i++) {
 			ArrayList<Integer> region_items = new ArrayList<Integer>(7);
 			ArrayList<Double> region_vals = new ArrayList<Double>(7);
-			for (int j = 0; j < 7; j++) {
-				Set<Integer> singleton = new HashSet<Integer>();
+			for (int j = 0; j < 7; j++) { //for each item in region
+				Set<Integer> singleton = new HashSet<Integer>(); //
 				singleton.add(i + 14*j);
 				region_items.add(i + 14*j);
 				region_vals.add(this.queryValue(singleton)); 
@@ -55,10 +55,9 @@ public class CombAuctionAgent extends AbsCombinatorialProjectAgentV2 {
 			regions.add(new Region(region_items, region_vals, this.queryValue(new HashSet<Integer>(region_items))));
 		}
 		
-		Map<Set<Integer>, Double> bestSkeletons = new HashMap<Set<Integer>, Double>();
-		PriorityQueue<Bundle> skeletonQueue = new PriorityQueue<Bundle>(1000, Collections.reverseOrder());
+		Set<Bundle> bestBundles = new HashSet<Bundle>();
 		
-		Map<Set<Integer>, Double> every14 = new HashMap<Set<Integer>, Double>();
+		PriorityQueue<Bundle> skeletonQueue = new PriorityQueue<Bundle>(1000, Collections.reverseOrder());
 		
 		for (int i = 0; i < 7; i++) {
 			Set<Integer> initialSet = new HashSet<Integer>();
@@ -66,23 +65,14 @@ public class CombAuctionAgent extends AbsCombinatorialProjectAgentV2 {
 				initialSet.add(14*i + j);
 			}
 			double value = this.queryValue(initialSet);
-			
-			
-			double sumSamples = 0;
-			
-			for (int j = 0; j<100; j++){
-				sumSamples += this.sampleValue(initialSet);
-			}
-			
-			double mean = sumSamples/100;
-			double initialDiff = value - mean;
-			
-			
-			Bundle initial = new Bundle(initialSet, value, initialDiff);
-			skeletonQueue.add(initial);
+//			if(i==0||i==3||i==6){
+//				Bundle initial = new Bundle(initialSet, value, 0);
+//				bestBundles.add(initial);
+//			}
 		}
 		
-		while ((System.currentTimeMillis() - tStart)/1000 < 15) {
+		
+		while ((System.currentTimeMillis() - tStart) < 18700) {
 			// generate one random skeleton 
 			Set<Integer> set = new HashSet<Integer>();
 			for (int j = 0; j < 14; j++) {
@@ -95,133 +85,41 @@ public class CombAuctionAgent extends AbsCombinatorialProjectAgentV2 {
 			
 			double sumSampleValues = 0;
 			
-			for (int i = 0; i< 100; i++){
+			for (int i = 0; i< 3; i++){
 				sumSampleValues += this.sampleValue(set);
 			}
 			
-			double meanSampleValue = sumSampleValues/100;
+			double meanSampleValue = sumSampleValues/3;
 			double difference = setValue - meanSampleValue;
 			Bundle current = new Bundle(set, setValue, meanSampleValue);
 			skeletonQueue.add(current);
 		}
+		System.out.println(skeletonQueue.size());
 		int counter = 0;
-		while ((System.currentTimeMillis() - tStart)/1000 < 19 && counter<40) {
-			for (int i = 0; i<40; i++){
+		
+		while ((System.currentTimeMillis() - tStart) < 19000 && counter<10) {
 				Bundle currentBest = skeletonQueue.poll();
-				bestSkeletons.put(currentBest.getBundle(), currentBest.getValue());
+				Bundle altered = new Bundle(currentBest.getBundle(), currentBest.getValue(), 0);
+				System.out.println(currentBest.getValue() + " : " + (currentBest.getValue()-currentBest.getPrice()));
+//				System.out.println(altered.getValue());
+				bestBundles.add(altered);
 				counter++;
-			}
 		}
-//		*/
+		Set<Integer> itemSet = new HashSet<Integer>();
+		for(Bundle bundle: bestBundles){
+			itemSet.addAll(bundle.getBundle());
+		}
+		System.out.println(itemSet.toString());
+		System.out.println(itemSet.size());
 
-		/*
-		Map<Integer, Set<Integer>> modulo14 = new HashMap<Integer, Set<Integer>>();
-		Map<Integer, Set<Integer>> every14 = new HashMap<Integer, Set<Integer>>();
-		for (int i = 0; i < 14; i++) {
-			modulo14.put(i, new HashSet<Integer>());
-		}
-		
-		for (int i = 0; i < 98; i++) {
-			Set<Integer> s = modulo14.get(i%14);
-			s.add(i);
-			modulo14.put(i%14,s);	
-		}
-		
-		for (int i = 0; i < 14; i++) {
-			System.out.println(this.queryValue(modulo14.get(i)));
-		}
-		System.out.println("RANDOM SAMPLES");
-		Map<Set<Integer>, Double> m = this.queryXORs(100, 7, 0);
-		Double sum = 0.0;
-		Double max = 0.0;
-		for (Double d : m.values()) {
-			sum += d;
-			max = (max < d) ? d : max;
-		}
-		System.out.println(sum / 100.0);
-		System.out.println(max);
-		*/
-		
-		
-		// Fixed chunks
-		Map<Set<Integer>, Double> fixed_sets = new HashMap<Set<Integer>, Double>();
-		/*
-		Set<Integer> one = new HashSet<Integer>();
-		Set<Integer> one_two = new HashSet<Integer>();
-		Set<Integer> two_three = new HashSet<Integer>();
-		Set<Integer> three = new HashSet<Integer>();
-		Set<Integer> two_four = new HashSet<Integer>();
-		*/
-		for (int i = 0; i < 7; i++) {
-			Set<Integer> s = new HashSet<Integer>();
-			for (int j = 0; j < 14; j++) {
-				s.add(14*i + j);
-			}
-			fixed_sets.put(s, this.queryValue(s));
-			/*
-			if (i == 0) {
-				one.addAll(s);
-				one_two.addAll(s);
-			}
-			
-			if (i == 1) {
-				one_two.addAll(s);
-				two_three.addAll(s);
-				two_four.addAll(s);
-			}
-			
-			if (i == 2) {
-				three.addAll(s);
-				two_three.addAll(s);
-			}
-			
-			if (i == 3) {
-				two_four.addAll(s);
-			}
-			*/
-		}
-		/*
-		System.out.println("Bundle : " + one.toString() + " Value : " + this.queryValue(one));
-		System.out.println("Bundle : " + one_two.toString() + " Value : " + this.queryValue(one_two));
-		System.out.println("Bundle : " + three.toString() + " Value : " + this.queryValue(three));
-		System.out.println("Bundle : " + two_three.toString() + " Value : " + this.queryValue(two_three));
-		System.out.println("Bundle : " + two_four.toString() + " Value : " + this.queryValue(two_four));
-		*/
-		
-		// Initialize random skeletons
-		Map<Set<Integer>, Double> random_skeletons = new HashMap<Set<Integer>, Double>();
-		for (int i = 0; i < 100; i++) {
-			Set<Integer> s = new HashSet<Integer>();
-			for (int j = 0; j < 14; j++) {
-				s.add(regions.get(j).getRandomItem());
-			}
-			random_skeletons.put(s, this.queryValue(s));
-		}
-		
-		
-		
-		
-		System.out.println("FIXED CHUNKS");
-		fixed_sets.forEach((key, value) -> {
-		    System.out.println("Bundle : " + key.toString() + " Value : " + value + " Sample Value : " + this.sampleValue(key) + " Sample Value : " + this.sampleValue(key));
-		});
-		System.out.println("RANDOM 14");
-		random_skeletons.forEach((key, value) -> {
-		    System.out.println("Bundle : " + key.toString() + " Value : " + value + " Sample Value : " + this.sampleValue(key) + " Sample Value : " + this.sampleValue(key));
-		});
-		
-		/*
-		System.out.println("RANDOM SAMPLES");
-		Map<Set<Integer>, Double> m = this.queryXORs(100, 14, 0);
-		Double sum = 0.0;
-		Double max = 0.0;
-		for (Double d : m.values()) {
-			sum += d;
-			max = (max < d) ? d : max;
-		}
-		System.out.println(sum / 100.0);
-		System.out.println(max);
-		*/
+//		System.out.println("FIXED CHUNKS");
+//		fixed_sets.forEach((key, value) -> {
+//		    System.out.println("Bundle : " + key.toString() + " Value : " + value + " Sample Value : " + this.sampleValue(key) + " Sample Value : " + this.sampleValue(key));
+//		});
+//		System.out.println("RANDOM 14");
+//		random_skeletons.forEach((key, value) -> {
+//		    System.out.println("Bundle : " + key.toString() + " Value : " + value + " Sample Value : " + this.sampleValue(key) + " Sample Value : " + this.sampleValue(key));
+//		});
 	}
 
 	@Override
@@ -257,6 +155,10 @@ public class CombAuctionAgent extends AbsCombinatorialProjectAgentV2 {
 		return possibleBids.last().bundle;
 	}
 	
+	private int getRegionID(Integer item) {
+		return item % 14;
+	}
+	
 	public static void main(String[] args) {
 		try {
 			new CombAuctionAgent("localhost", 2424);
@@ -267,5 +169,7 @@ public class CombAuctionAgent extends AbsCombinatorialProjectAgentV2 {
 			e.printStackTrace();
 		}
 	}
+	
+	
 
 }
